31429cc058acd4646edcd88ebf2eee15fa8a7715
luis <diazdiaz12126@gmail.om>
2020-12-08 23:58:30 -0500

    validacion2.0

--- BD/validacion.php
+++ BD/validacion.php
@@ -5,15 +5,19 @@ require_once '../BD/conexion.php';
 $consulta="SELECT *FROM usuarios WHERE nombre='$nombr' AND  contrasena='$contr'";
 $query = mysqli_query($conexion,$consulta);
 
+$fias=mysqli_fetch_array($query);
 
-
-$fias=mysqli_num_rows($query);
-
-if ($fias>0){
+if ($fias['id_cargoo']==1){
 session_start();
 $_SESSION['login']="Administrador";
-echo "Bienvenido $nombr";
+echo "admin";
 }
+else
+if ($fias['id_cargoo']==2) {
+	echo "Programador";
+}
+
+
 else{
 	echo "<script>
           alert('No tienes acceso al sistema');
