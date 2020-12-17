9a026d0aa75f760cc65761a5e44896e41c3fc549
luis <diazdiaz12126@gmail.om>
2020-12-17 00:14:44 -0500

    registro

--- BD/NuevoRegistro.php
+++ BD/NuevoRegistro.php
@@ -9,7 +9,18 @@ $contraseña=$_POST['contra'];
 $cargo =$_POST['aqui se haria del select'];
 
 $Insertar="INSERT INTO usuarios VALUES ('$nombre','$apellidom','$apellidop','$contraseña','$cargo')";
+$consulta=mysqli_query($conexion,$Insertar);
 
+if(!$consulta){
+    echo "<script>
+    alert('Ocurrio algun error en el registro');
+    </script>"
+}
+else {
+    echo "<script>
+    alert('registro realizado con exito');
+    </script>"
+}
 
 ?>
 
