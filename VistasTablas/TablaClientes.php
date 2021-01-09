fd161fce4b0b1b5a913900ae4e77e66237aeb838
Alejandro-DP <alexdelacruz1888@gamil.com>
2021-01-09 00:09:03 -0600

    estilos dash in tables

--- VistasTablas/TablaClientes.php
+++ VistasTablas/TablaClientes.php
@@ -4,14 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/wrapper.css">
     <link rel="stylesheet" href="../DT/DataTables-1.10.23/css/dataTables.bootstrap4.min.css">
     <title></title>
 </head>
 <body>
 
-
-<form method="POST">
-<table id="tabla" class="table-responssive table-striped table-bordered" style="width:100%">
+	
+<div class="user-var">
+  <div class="logodb">
+    <img src="../Recursos/thelogo.png">
+    
+  </div>
+  <div class="user">
+    <div class="img">
+     <img src="../Recursos/perfildeusuario.jpg" class="user-logo" >
+    </div>
+   
+    <p class="text-right" id="name">
+    <?php 
+     session_start();
+     $nom=$_SESSION['login']  ;    
+     echo "  Bienvenido $nom   ";
+  ?>
+    </p>
+  </div>
+</div>
+<div class="card-body">
+<form method="POST" class="show-t">
+<table id="tabla" class="table-responssive table-striped table-bordered" >
 <thead class="thead-light"> 
     
         <th>ID CLIENTE</th>
@@ -81,6 +102,47 @@ if(isset($_POST['borrar'])){
 
 
 </form>
+</div>
+<div  class="menu">
+  <h5 class = "icon-dat">
+    bases de datos
+</h5>
+<div class="data-bases">
+  <tr class="data">
+    <?php
+    require '../BD/conexion.php';
+    $sentenciasql="SHOW DATABASES";
+    $consulta=mysqli_query($conexion,$sentenciasql);
+    while($otra=mysqli_fetch_row($consulta)){
+      echo "
+      <tr >
+      {$otra[0]}
+      </tr>
+      <br>
+      ";
+    }
+
+  ?>
+  </tr>
+  <div class="tables">
+    <tr>
+
+    </tr>
+  </div>
+</div>
+  <div class="button-flo" onclick="togle()">
+    <img class="opc"src="../Recursos/more.png" >
+  </div>
+  <div class="col-md-4 col-md-offset-1" id="opciones">
+    
+      <li><a>Importar</a></li>
+      <li ><a href="#">Exportar</a></li>
+      <li><a href="UserView.php">Cuentas de Usuario</a></li>
+      <li><a>Bases de Datos</a></li>
+    
+  </div>
+</div>
+
 
 <script src="../js/jquery.js"></script>
 <script src="../js/bootstrap.min.js"></script>
