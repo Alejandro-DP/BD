139d3cfbd24ed60459a586af00865578d35cff64
luis <diazdiaz12126@gmail.om>
2021-01-09 19:19:41 -0500

    bd

---
+++ BD/nuevabd.php
@@ -0,0 +1,63 @@
+<?php
+require '../BD/conexion.php';
+
+$nombre=$_POST["nombre"];
+$bd=$_POST["bd"];
+
+if (empty($nombre) || empty($bd)){
+echo "
+
+<script>
+alert('no puedes dejar campos vacios');
+location.href='../VistasAdmin/Importar.php';
+</script>;
+";
+}
+
+else{
+    $query="CREATE DATABASE $nombre";
+    $query2=mysqli_query($conexion,$query);
+    if(!$query2){
+        echo "
+        <script>
+        alert('error al crear la bd');
+
+        </script>;
+        ";
+    }
+    else {
+
+        echo "
+
+        <script>
+        alert('Base de datos creada');
+        location.href='../VistasAdmin/Importar.php';
+        </script>;
+        
+        "
+        ;
+    
+    
+
+
+
+}
+
+/*
+$usarbd="USE 'bd'";
+$consi=mysqli_query($conexion,$usarbd);
+$imp="SOURCE '$bd'";
+$seti=mysqli_query($conexion,$imp);
+if (!$seti){
+    echo "fallo";
+}
+else{
+    echo "bien";
+}*/
+
+}
+
+
+
+
+?>
