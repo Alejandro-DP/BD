a9c98f1cf423ae0524d9720fb84d97b0e7e17288
luis <diazdiaz12126@gmail.om>
2021-01-10 13:20:55 -0500

    agregue el dom a la vista importar

--- VistasAdmin/Importar.php
+++ VistasAdmin/Importar.php
@@ -49,6 +49,35 @@
 </table>
 </div>
 <div  class="menu">
+
+<h5 class = "icon-dat">
+    bases de datos
+</h5>
+<div class="data-bases">
+  <?php
+require '../BD/conexion.php';
+$sentenciasql="SHOW DATABASES";
+$s="SHOW TABLES IN Air";
+$consulta=mysqli_query($conexion,$sentenciasql);
+
+while($otra=mysqli_fetch_row($consulta)){
+$dom = new DOMDocument('1.0', 'utf-8');
+$element = $dom->createElement('li',"{$otra[0]}");
+
+// Insertamos el nuevo elemento como raíz (hijo del documento)
+$dom->appendChild($element);
+echo $dom->saveXML();
+}
+
+
+
+?>
+  <div class="tables">
+    <tr>
+
+    </tr>
+  </div>
+</div>
   <div class="button-flo" onclick="togle()">
     <p class="opc"> +</p>
   </div>
