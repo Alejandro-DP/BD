f94754b5a1bc73395c37657b3e3aac6a4b84ecdf
luis <diazdiaz12126@gmail.om>
2021-01-10 13:40:05 -0500

    DOM EN CLIENTE

--- VistasTablas/TablaClientes.php
+++ VistasTablas/TablaClientes.php
@@ -97,26 +97,29 @@ if(isset($_POST['borrar'])){
 </form>
 </div>
 <div  class="menu">
-  <h5 class = "icon-dat">
+<h5 class = "icon-dat">
     bases de datos
 </h5>
 <div class="data-bases">
-  <tr class="data">
-    <?php
-    require '../BD/conexion.php';
-    $sentenciasql="SHOW DATABASES";
-    $consulta=mysqli_query($conexion,$sentenciasql);
-    while($otra=mysqli_fetch_row($consulta)){
-      echo "
-      <tr >
-      {$otra[0]}
-      </tr>
-      <br>
-      ";
-    }
+  <?php
+require '../BD/conexion.php';
+$sentenciasql="SHOW DATABASES";
+$s="SHOW TABLES IN Air";
+$consulta=mysqli_query($conexion,$sentenciasql);
 
-  ?>
-  </tr>
+while($otra=mysqli_fetch_row($consulta)){
+$dom = new DOMDocument('1.0', 'utf-8');
+$element = $dom->createElement('li',"{$otra[0]}");
+
+// Insertamos el nuevo elemento como raíz (hijo del documento)
+$dom->appendChild($element);
+echo $dom->saveXML();
+}
+
+
+
+?>
+  
   <div class="tables">
     <tr>
 
